c2406e27b666d4c0b0181dcf8de9831e74422d12
Sofya Bushkovskaya <sonya.yudina17@mail.ru>
2024-10-11 23:20:26 +0300

    add in admin.py search_fields to Category

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,6 +5,10 @@ from catalog.models import Category, Product
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
+    search_fields = (
+        "name",
+        "description",
+    )
 
 
 @admin.register(Product)
