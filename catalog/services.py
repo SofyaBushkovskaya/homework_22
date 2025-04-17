70d153cb93397fb09d1f4e8753345f079e796da5
SofyaBushkovskaya <sonya.yudina17@mail.ru>
2025-04-17 13:12:03 +0300

    add services catalog

---
+++ catalog/services.py
@@ -0,0 +1,17 @@
+from django.core.cache import cache
+
+from catalog.models import Product
+
+from config.settings import CACHES_ENABLED
+
+def get_products_from_cache():
+    """Получает данные по продуктам из кеша, если кеш пуст, получает данные из базы данных."""
+    if not CACHES_ENABLED:
+        return Product.objects.all()
+    key = "products_list"
+    products = cache.get(key)
+    if products is not None:
+        return products
+    products = Product.objects.all()
+    cache.set(key, products)
+    return products
