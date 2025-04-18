56ebfaedb7beccc20efde89e77d10ed8e7a4e4c9
SofyaBushkovskaya <sonya.yudina17@mail.ru>
2025-04-18 14:37:12 +0300

    edit services

--- catalog/services.py
+++ catalog/services.py
@@ -1,6 +1,6 @@
 from django.core.cache import cache
 
-from catalog.models import Product
+from catalog.models import Product, Category
 
 from config.settings import CACHES_ENABLED
 
@@ -13,5 +13,20 @@ def get_products_from_cache():
     if products is not None:
         return products
     products = Product.objects.all()
+    cache.set(key, products)
+    return products
+
+def get_products_by_category_from_cache(category_id):
+    """Получает данные по продуктам в указанной категории из кеша, если кеш пуст, получает данные из базы данных."""
+    if not CACHES_ENABLED:
+        return Category.objects.filter(category_id=category_id)
+
+    key = f"products_list_category_{category_id}"
+    products = cache.get(key)
+
+    if products is not None:
+        return products
+
+    products = Category.objects.filter(category_id=category_id)
     cache.set(key, products)
     return products
